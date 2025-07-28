3692dcd7974098c3e8e881f7a980b72cc7d26e8d
Mirco Bozzolini <mirco.bozzolini@lantechlongwave.it>
2025-07-28 19:02:49 +0200

    fix(sns): corretta issue lunghezza DisplayName closes #36

--- sns.tf
+++ sns.tf
@@ -1,6 +1,12 @@
+locals {
+  sns_display_text = "Pipeline notification topic "
+  # This is needed since SNS DisplayName attribute is limited to 100 chars
+  final_display_name = length("${local.sns_display_text} ${local.final_name}") > 100 ? "${local.sns_display_text} ${substr(local.final_name, 0, 100 - (length(local.sns_display_text) + 1))}" : "${local.sns_display_text} ${local.final_name}"
+}
+
 resource "aws_sns_topic" "pipeline_notifications" {
   name_prefix  = length("${local.final_name}${local.codepipeline_resources_suffix}") > 38 ? substr("${local.final_name}${local.codepipeline_resources_suffix}", 0, 37) : "${local.final_name}${local.codepipeline_resources_suffix}"
-  display_name = "Notification topic about the pipeline in ${var.repo_org}/${var.repo_name} branch ${var.repo_branch}"
+  display_name = local.final_display_name
 }
 
 resource "aws_sns_topic_subscription" "pipeline_notifications_lambda" {
